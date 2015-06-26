923bc3b7756b6383adb313d8517c063e0c404f4d
daniel <daniel.ppr97@hotmail.com>
2015-06-25 21:35:04 -0300

    aula 25 06

---
+++ danni_25_06/newEmptyPHP.php
@@ -0,0 +1,163 @@
+
+
+<html>
+    <head>
+
+        <title>Eta sozin</title>
+        <style> 
+           
+        </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+            <link href="css/docs.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+        <style>
+            .carousel-inner img{
+                margin: auto
+
+            }
+        </style>
+    </head>
+
+
+    <body>         
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="../" class="navbar-brand">Salão</a>
+            </div>
+            <nav id="bs-navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="../getting-started/">Novidades</a>
+                    </li>
+                    <li>
+                        <a href="../css/">Importados</a>
+                    </li>
+                    <li>
+                        <a href="../components/">Ofertas</a>
+                    </li>
+                    <li class="active">
+                        <a href="../javascript/"> Fale conosco</a>
+                    </li>
+                    <li>
+                        <a href="../customize/">Mais vendidos</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="http://expo.getbootstrap.com" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Expo');">Expo</a></li>
+                    <li><a href="http://blog.getbootstrap.com" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Blog');">Blog</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="bs-docs-header" id="content" tabindex="-1">
+            <div class="container">
+                <h1>Sah Veiculos</h1>
+                <p>O site com maior número de vendas da região,os melhores preços e as maiores ofertas!</p>
+                <div id="carbonads-container"><div class="carbonad"><div id="azcarbon"></div><script>var z = document.createElement("script");
+                    z.async = true;
+                    z.src = "http://engine.carbonads.com/z/32341/azcarbon_2_1_0_HORIZ";
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(z, s);</script></div></div>
+
+            </div>
+        </div>
+
+
+
+
+
+        <!--<div class="wrapper">-->
+        <div class="header">
+
+
+        </div>
+
+
+
+
+        <div class="container">
+
+            <form class="form-inline">
+                <div class="form-group">
+                    <label for="exampleInputName2">Login</label>
+                    <input type="text" class="form-control" id="exampleInputName2" placeholder="Login" >
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputPassword3">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-default">Logar</button>
+            </form>
+
+
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="Img/tunado.jpg" alt="...">
+                        <div class="carousel-caption">
+                            ...
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="Img/camaro.jpg" alt="650">
+
+                        <div class="carousel-caption">
+                            ...
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <img src="Img/porsche.jpg" alt="650">
+
+                        <div class="carousel-caption">
+                            ...
+                        </div>
+                    </div>
+                    ...
+                </div>
+                
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <div>  <img src="Img/tracker.pg.jpg" alt="800">  <img src="Img/cobalt.pg.jpg" alt="800">      
+
+                     
+
+            
+            
+            
+            <div> 
+
+
+
+
+
+    </body>
+</html>
