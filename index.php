503edf33bcbfa9b72fdebb5093cc33ffc4c6b23b
daniel <daniel.ppr97@hotmail.com>
2015-06-30 21:28:05 -0300

    30 06

--- index.php
+++ index.php
@@ -1,21 +1,111 @@
 
+
 <html>
     <head>
-        <title>TODO supply a title</title>
+        
+        
+
+        <title>Eta sozin</title>
+        <style> 
+           
+        </style>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+            <link href="css/docs.min.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
-    </head>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <style>
+            .carousel-inner img{
+                margin: auto
 
-    <body>
+            }
+        </style>
+       
+    </head>  
+    
+
+
+    <body>        
         <div class="container">
-            <div class="row">
-                <div class="col-md-6">.col-md-6</div>
-                <div class="col-md-6">.col-md-6</div>
+            <div class="navbar-header">
+                <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="../" class="navbar-brand">Salão</a>
             </div>
+            <nav id="bs-navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="../getting-started/">Novidades</a>
+                    </li>
+                    <li>
+                        <a href="../css/">Importados</a>
+                    </li>
+                    <li>
+                        <a href="../components/">Ofertas</a>
+                    </li>
+                    <li class="active">
+                        <a href="../javascript/"> Fale conosco</a>
+                    </li>
+                    <li>
+                        <a href="../customize/">Mais vendidos</a>
+                    </li>
+                </ul>
+                
+            </nav>
+           
+
+        </div>
+        <div class="bs-docs-header" id="content" tabindex="-1">
+            <div class="container">
+                <h1>Speed Veículos</h1>
+                <p>O site com maior número de vendas da região,os melhores preços e as maiores ofertas!</p> 
+                <div id="carbonads-container"><div class="carbonad"><div id="azcarbon"></div><script>var z = document.createElement("script");
+                    z.async = true;
+                   
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(z, s);</script></div></div>
+                
+                
+                <center>   <input type="text" id="txtBusca" placeholder="Pesquisar"/> </center>
+
+
+            </div>
+        </div>
+
+
+
+
+
+        <!--<div class="wrapper">-->
+        <div class="header">
+
+
+        </div>
+
+
+
+
+        <div class="container">
+
+            <form class="form-inline">
+                <div class="form-group">
+                    <label for="exampleInputName2">Login</label>
+                    <input type="text" class="form-control" id="exampleInputName2" placeholder="Login" >
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputPassword3">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-default">Entrar</button>
+            </form>
+
 
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                 <!-- Indicators -->
@@ -28,19 +118,29 @@
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner" role="listbox">
                     <div class="item active">
-                        <img src="Img/fusion.jpg" alt="650">
+                        <img src="Img/camaro.jpg" alt="...">
                         <div class="carousel-caption">
                             ...
                         </div>
                     </div>
                     <div class="item">
-                        <img src="Img/ecosport.jpg" alt="650">
+                        <img src="Img/camaro.jpg" alt="650">
+
+                        <div class="carousel-caption">
+                            ...
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <img src="Img/porsche.jpg" alt="650">
+
                         <div class="carousel-caption">
                             ...
                         </div>
                     </div>
                     ...
                 </div>
+                
 
                 <!-- Controls -->
                 <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
@@ -53,10 +153,26 @@
                 </a>
             </div>
         </div>
-    </body>
+        <br>
+        <br>
+    <center><h1>Carros importados</h1></center>
+        <div>  <CENTER><img src="Img/tracker.pg.jpg"  alt="800"></CENTER>  
+            <br>
+            <br>
+            <br>
+            <center> <h1>Nacionais</h1></center>
+            <CENTER> <img src="Img/cobalt.pg.jpg" alt="800"></CENTER> 
 
+                     
+
+            
+            
+            
+            <div> 
+
+ 
+
+
+
+    </body> 
 </html>
-
-
-
-
